3d981f7904b42973888798a7a78a01832830821d
Sebastian Alejandro Suarez <sas.tuc@gmail.com>
2020-06-08 08:08:59 -0300

    MSG006 - Add minesweeper logic for putting mines on board

--- src/main/java/com/sas/minesweeper/service/game/MinesweeperGameService.java
+++ src/main/java/com/sas/minesweeper/service/game/MinesweeperGameService.java
@@ -1,5 +1,7 @@
 package com.sas.minesweeper.service.game;
 
+import static com.sas.minesweeper.util.CellContent.BOMB;
+
 import com.sas.minesweeper.controller.request.NewGameRequest;
 import com.sas.minesweeper.controller.response.GameResponse;
 import com.sas.minesweeper.entities.Board;
@@ -7,11 +9,18 @@ import com.sas.minesweeper.entities.model.GameBoard;
 import com.sas.minesweeper.mapper.BoardGameMapper;
 import com.sas.minesweeper.service.GameService;
 import com.sas.minesweeper.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MinesweeperGameService implements GameService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MinesweeperGameService.class);
+
+    private static final int ZERO_INDEX = 0;
+    private static final int ONE_INDEX = 1;
 
     @Autowired
     private UserService userService;
@@ -21,12 +30,17 @@ public class MinesweeperGameService implements GameService {
 
     @Override
     public GameResponse newGame(String username, NewGameRequest newGameRequest) {
+        logger.info("Creating a new game rows: {} columns: {} bombs: {} for user {}", newGameRequest.getRow(), newGameRequest.getColumn(), newGameRequest.getBombs(), username);
+
         Board board = new Board(newGameRequest.getRow(), newGameRequest.getColumn(), newGameRequest.getBombs());
+        putBombs(board);
 
         GameBoard gameBoard = boardGameMapper.boardToGameBoard(board);
         gameBoard.newGameBoard();
 
         userService.saveNewGame(username, gameBoard);
+
+        logger.info("Creating a new game for user {} successful", username);
 
         return GameResponse.builder()
                 .createdDate(gameBoard.getCreatedDate())
@@ -34,5 +48,43 @@ public class MinesweeperGameService implements GameService {
                 .timeSpend(0)
                 .board(board.getMaskedBoard())
                 .build();
+    }
+
+    private void putBombs(Board board) {
+        for (int i = ZERO_INDEX; i < board.getBombsNumber();) {
+            int randomColumn = random(board.getColumnsNumber());
+            int randomRow = random(board.getRowsNumber());
+
+            if (board.getCell(randomRow, randomColumn) != BOMB) {
+                board.putMine(randomRow, randomColumn);
+                fillNeighborhood(board, randomRow, randomColumn);
+                i++;
+            }
+        }
+    }
+
+    private void fillNeighborhood(Board board, int randomRow, int randomColumn) {
+        int minColumn = getMin(randomColumn);
+        int maxColumn = getMax(randomColumn, board.getColumnsNumber());
+        int minRow = getMin(randomRow);
+        int maxRow = getMax(randomRow, board.getRowsNumber());
+
+        for (int i = minRow; i <= maxRow; i++) {
+            for (int j = minColumn; j <= maxColumn; j++) {
+                if (board.getCell(i, j) != BOMB) {
+                    board.increaseCellValue(i, j);
+                }
+            }
+        }
+    }
+
+    private int random(int columnsNumber) {
+        return (int) (Math.random() * columnsNumber);
+    }
+    private int getMin(int number) {
+        return number == ZERO_INDEX ? number : number - ONE_INDEX;
+    }
+    private int getMax(int number, int max) {
+        return number + ONE_INDEX >= max ? number : number + ONE_INDEX;
     }
 }
---
+++ src/main/java/com/sas/minesweeper/util/CellContent.java
@@ -0,0 +1,7 @@
+package com.sas.minesweeper.util;
+
+public class CellContent {
+    public static final int MASKED_CELL = -2;
+    public static final int BOMB = -1;
+    public static final int NOTHING = 0;
+}
