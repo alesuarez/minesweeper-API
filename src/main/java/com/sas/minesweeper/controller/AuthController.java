81af1990d93dffa25649491e524aebc5b1d12a9d
Alejandro Suarez <sas.tuc@gmail.com>
2021-04-07 20:13:21 -0300

    comentario

--- src/main/java/com/sas/minesweeper/controller/AuthController.java
+++ src/main/java/com/sas/minesweeper/controller/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @CrossOrigin("http://localhost:3000")
 public class AuthController {
-
+// comentario vacio
     @Autowired
     private AuthService authService;
 
